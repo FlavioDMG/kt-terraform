bb757534b5e939d450b42bad1910f045846dcdae
FlavioDMG <65253457+FlavioDMG@users.noreply.github.com>
2022-02-17 20:37:32 -0300

    Delete locals.tf

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  subnet_ids = { for k, v in aws_subnet.this : v.tags.Name => v.id }
-
-  cammon_tags = {
-    Project = "RegraEntrada porta 80 e 22, RegraSaida tudo"
-    Owner   = "Flavio Gutierrez"
-  }
-}
