39e21aa5a0c91b0ff154d7aff7357eaa0c9d6ce0
FlavioDMG <65253457+FlavioDMG@users.noreply.github.com>
2022-02-18 13:18:21 -0300

    Update outputs.tf

--- S3/outputs.tf
+++ S3/outputs.tf
@@ -1,7 +1,7 @@
-output "Criamos_um_bucket_para_armazenar_nosso_tfstate" {
+output "Criado_um_bucket_para_armazenar_meu_tfstate" {
   value = aws_s3_bucket.bucket.id
 }
 
-output "Ativamos_bloqueio_de_acesso_ao_nosso_repositorio_publico" {
+output "Ativado_bloqueio_de_acesso_para_publico_ao_meu_repositorio" {
   value = aws_s3_bucket_public_access_block.bloquearpublico.block_public_policy
 }
